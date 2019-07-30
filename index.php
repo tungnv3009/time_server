d63e31954beb7e928a9b7785cc7b4ead4c7a868c
Tùng Tùng <ngovantungcoi@gmail.com>
2019-07-30 10:40:32 +0700

    bai tap

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "xin chao";
+?>
+<html>
+    <head>
+       <title>Thời gian hiện tại</title>
+    </head>
+    <body>
+        <h1> Bây giờ là: <?php echo date('Y-M-d h:m:s'); ?> </h1>
+    </body>
+</html>
